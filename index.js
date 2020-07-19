8283cab94e3504fabb9d5c7f32ffaec6ddc2c102
Nick Clifford <nick@nickclifford.me>
2020-07-19 12:15:09 -0500

    first pass on recommended rules

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ module.exports = {
 	],
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended'
-	]
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking'
+	],
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
+		'@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-call': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off'
+	}
 };
