3c21d7d95c1b214dfc4abe94cdd1847b44dbd100
Nick Clifford <nick@nickclifford.me>
2020-07-21 10:56:30 -0500

    remove extra script

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
 	plugins: [
-		'@typescript-eslint'
+		'@typescript-eslint',
+		'sort-imports-es6-autofix'
 	],
 	extends: [
 		'eslint:recommended',
@@ -10,11 +11,30 @@ module.exports = {
 	],
 	rules: {
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-extra-parens': 'warn',
 		'@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
 		'@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
 		'@typescript-eslint/no-non-null-assertion': 'off',
 		'@typescript-eslint/no-unsafe-assignment': 'off',
 		'@typescript-eslint/no-unsafe-call': 'off',
-		'@typescript-eslint/no-unsafe-member-access': 'off'
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+		'eqeqeq': 'error',
+		'no-duplicate-imports': 'warn',
+		'no-else-return': 'warn',
+		'no-eval': 'error',
+		'no-lonely-if': 'warn',
+		'no-restricted-syntax': ['error', 'ForInStatement'], // based on AST nodes
+		'no-return-await': 'warn',
+		'no-shadow': 'error',
+		'no-var': 'error',
+		'object-shorthand': 'error',
+		'one-var': ['warn', 'never'],
+		'prefer-arrow-callback': 'error',
+		'prefer-const': 'error',
+		'radix': 'error',
+		'sort-imports-es6-autofix/sort-imports-es6': ['warn', {
+			ignoreCase: true,
+			memberSyntaxSortOrder: ["none", "multiple", "all", "single"]
+		}]
 	}
 };
